c514a918f44241bfbec9f183370a59c2bd5f4ee0
KevinMusiol <joda80@outlook.de>
2023-11-12 13:11:04 +0100

    Hinzufügen der Swagger Kommentare

--- src/main/java/de/szut/lf8_project/controllers/ProjectController.java
+++ src/main/java/de/szut/lf8_project/controllers/ProjectController.java
@@ -10,6 +10,10 @@ import de.szut.lf8_project.services.EmployeeAssocService;
 import de.szut.lf8_project.services.EmployeeService;
 import de.szut.lf8_project.services.MappingService;
 import de.szut.lf8_project.services.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -18,7 +22,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "api/pms/project")
 public class ProjectController {
 
     private final ProjectService projectService;
@@ -35,12 +39,26 @@ public class ProjectController {
     }
 
     // Project
-
+    @Operation(summary = "Create a project")
+    @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Project created"),
+    @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+    @ApiResponse(responseCode = "400", description = "Wrong id format", content = @Content),
+    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @PostMapping("/projects")
     public GetProjectDTO create(@RequestBody @Valid CreateProjectDTO dto) {
         return mappingService.mapProjectEntityToGetProjectDTO(projectService.create(mappingService.mapCreateProjectDTOToProjectEntity(dto)));
     }
 
+    @Operation(summary = "Update a project")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Project updated"),
+            @ApiResponse(responseCode = "404", description = "Project not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Wrong id format", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @PostMapping("/projects/{id}")
     public GetProjectDTO update(@RequestBody @Valid CreateProjectDTO dto, @PathVariable Long id) {
         ProjectEntity entity = projectService.update(mappingService.mapCreateProjectDTOToProjectEntity(dto, id));
@@ -49,7 +67,14 @@ public class ProjectController {
         }
         return mappingService.mapProjectEntityToGetProjectDTO(entity);
     }
-
+    @Operation(summary = "Find a project by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Project found"),
+            @ApiResponse(responseCode = "404", description = "Project not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Wrong id format", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @GetMapping("/projects/{id}")
     public GetProjectDTO findById(@PathVariable Long id) {
         ProjectEntity entity = projectService.findById(id);
@@ -58,12 +83,24 @@ public class ProjectController {
         }
         return mappingService.mapProjectEntityToGetProjectDTO(entity);
     }
-
+    @Operation(summary = "Find all projects")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Projects found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @GetMapping("/projects")
     public List<GetProjectDTO> findAll() {
         return projectService.findAll().stream().map(mappingService::mapProjectEntityToGetProjectDTO).toList();
     }
-
+    @Operation(summary = "Delete a project")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Project deleted"),
+            @ApiResponse(responseCode = "404", description = "Project not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Wrong id format", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @DeleteMapping("/projects/{id}")
     public GetProjectDTO delete(@PathVariable Long id) {
         ProjectEntity entity = projectService.delete(id, employeeAssocService);
@@ -74,7 +111,14 @@ public class ProjectController {
     }
 
     // Employee Assoc
-
+    @Operation(summary = "Find all employees from a project")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employees found"),
+            @ApiResponse(responseCode = "404", description = "Project not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Wrong id format", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @GetMapping("/employees/{id}/projects")
     public List<GetProjectDTO> getProjectsByEmployeeId(@PathVariable Long id, @RequestHeader("Authorization") String token) {
         try {
@@ -87,12 +131,26 @@ public class ProjectController {
                 .map(mappingService::mapProjectEntityToGetProjectDTO)
                 .toList();
     }
-
+    @Operation(summary = "Find all employees from a project")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employees found"),
+            @ApiResponse(responseCode = "404", description = "Project not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Wrong id format", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @GetMapping("/projects/{id}/employees")
     public List<GetEmployeeAssocDTO> getEmployeesByProjectId(@PathVariable Long id) {
         return mappingService.mapEmployeeAssocEntitiesToGetEmployeeAssocDTOs(employeeAssocService.getEmployeesFromProject(id));
     }
-
+    @Operation(summary = "Add an employee to a project")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee added to project"),
+            @ApiResponse(responseCode = "404", description = "Project or employee not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Wrong id format", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @PostMapping("/projects/{id}/employees")
     public GetProjectDTO addEmployeeToProject(@PathVariable Long id, @RequestHeader("Authorization") String token, @RequestBody @Valid CreateEmployeeAssocDTO dto) {
         ProjectEntity entity = projectService.findById(id);
@@ -109,8 +167,14 @@ public class ProjectController {
         employeeAssocService.addToProject(employeeAssocEntity);
         return mappingService.mapProjectEntityToGetProjectDTO(entity);
     }
-
-    @DeleteMapping("/projects/{id}/employees")
+    @Operation(summary = "Remove an employee from a project")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee removed from project"),
+            @ApiResponse(responseCode = "404", description = "Project or employee not found or employee does not work on project", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Wrong id format", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })@DeleteMapping("/projects/{id}/employees")
     public GetProjectDTO removeEmployeeToProject(@PathVariable Long id, @RequestBody String body) {
         try {
             ProjectEntity entity = projectService.findById(id);
@@ -126,8 +190,7 @@ public class ProjectController {
         }
     }
 
-    // Employee
-
+    // Employee Redirect
     @GetMapping("/**")
     public JsonNode employeeRedirectGet(@RequestHeader("Authorization") String token, HttpServletRequest request) {
         return employeeService.redirectGet(request.getRequestURI(), token, JsonNode.class);
